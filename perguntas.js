060fc4169f235801a82701bf9ab6cfd97d2364b9
riqui2006 <00001111747234sp@al.educacao.sp.gov.br>
2024-11-05 19:09:57 -0300

    Final

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python e para que serve?',
+    'O Python é uma linguagem de programação amplamente usada em aplicações da Web, desenvolvimento de software, ciência de dados e machine learning (ML).'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da califórnia?',
+    'A capital da califórnia é Sacramento'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Pichau',
+    'Como comprar mais barato na Pichau?',
+    'A grande maioria dos produtos vendidos na Pichau contam com um super desconto, geralmente de 15%, para pagamentos via Pix.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz O garoto e bravo em Inglês?',
+    'O garoto e bravo em ingles é The boy is angry'
+)
+
+criaCartao(
+    'Duvida',
+    'Para que serve Pichau?',
+    'Fundada no ano de 2007, a Pichau Informática é uma empresa que trabalha com os mais variados produtos, atuando principalmente no setor de informática e equipamentos eletrônicos em geral.'
+)
